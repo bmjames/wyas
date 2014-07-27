b10875c35dea5cb7d23dd20c3324f44ec2866c85
Ben James <bmjames@gmail.com>
2014-07-27 22:02:45 +0100

    Abort parsing multi-line statements in REPL after blank line

--- Main.hs
+++ Main.hs
@@ -9,28 +9,29 @@ import Data (LispVal)
 import Control.Monad (forever)
 import Data.Monoid   ((<>))
 import Data.Text     (Text, pack)
+import Data.Foldable (traverse_)
 import System.IO     (hFlush, stdout)
 
 import Data.Attoparsec.Text
 
-getInput :: String -> IO Text
+getInput :: String -> IO (Maybe Text)
 getInput prompt = do
   putStr prompt
   hFlush stdout
   line <- getLine
-  return $ pack line <> "\n"
+  return $ if null line then Nothing else Just $ pack line <> "\n"
 
 handleParseResult :: Result LispVal -> IO ()
 handleParseResult parseResult =
   case parseResult of
     Fail _ _ parseErr -> putStrLn $ "*** " ++ parseErr
-    Done _   val      -> 
+    Done _   val -> 
       let output = either show show result
           result = runEval nullEnv $ eval val
       in putStrLn output
-    Partial resume    -> getInput "... " >>= handleParseResult . resume
+    Partial resume ->
+      getInput "... " >>= traverse_ (handleParseResult . resume)
 
 main :: IO ()
 main = forever $
-  getInput ">>> " >>= handleParseResult . parse parseExpr
-
+  getInput ">>> " >>= traverse_ (handleParseResult . parse parseExpr)
