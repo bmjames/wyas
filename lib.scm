00693ea945e60e361ee38d05fffa80e44573ad3a
Ben James <ben.james@guardian.co.uk>
2014-08-01 22:39:21 +0100

    Implement some more library functions

--- lib.scm
+++ lib.scm
@@ -3,5 +3,24 @@
     x
     (f (car xs) (foldr f x (cdr xs)))))
 
+(define (foldl f x xs)
+  (if (null? xs)
+    x
+    (foldl f (f x (car xs)) (cdr xs))))
+
 (define (map f xs)
   (foldr (lambda (x ys) (cons (f x) ys)) '() xs))
+
+(define (filter f xs)
+  (foldr (lambda (x xs) (if (f x) (cons x xs) xs)) '() xs))
+
+(define (flip f)
+  (lambda (a b) (f b a)))
+
+(define (length xs)
+  (foldl (lambda (x y) (+ 1 x)) 0 xs))
+
+(define (reverse xs)
+  (foldl (flip cons) '() xs))
+
+(define (id x) x)
